c3808f59b4c850534c51ac8cade77b9aa8c8c1b4
Alina D <adalli@web.de>
2021-06-24 21:11:26 +0200

    Update for cropping relevant image parts

--- src/data/image_postprocessing.py
+++ src/data/image_postprocessing.py
@@ -1,18 +1,21 @@
-from pathlib import Path 
-import os 
-import json 
-import pandas as pd 
+from pathlib import Path
+import os
+import json
+import pandas as pd
 from src.data.mapillary import crop_image_flat
 
 
-def image_postprocessing(output_dir: str, image_df: pd.DataFrame):
-    """Represents a postprocessing function
+def crop_relevant_image_parts(output_dir: str, image_df: pd.DataFrame):
+    """Iterates over all images in the dataset and takes their
+    object detections to crop the image to only contain the relevant parts,
+    e.g. streets. The images are saved in a folder "cropped" in the given
+    dataset directory
 
     Args:
         output_dir (str): directory with saved dataset (containing
             'images' and 'object_detections' subfolders)
         image_df (pd.DataFrame): dataframe containing all mapillary keys
-            in the dataset 
+            in the dataset
     """
     cropped_dir = f"{output_dir}/cropped"
     Path(cropped_dir).mkdir(exist_ok=True)
@@ -22,7 +25,7 @@ def image_postprocessing(output_dir: str, image_df: pd.DataFrame):
 
     mapillary_keys = image_df["mapillary_key"].to_list()
 
-    # iterate over all keys in the dataset 
+    # iterate over all keys in the dataset
     for map_key in mapillary_keys:
         image = os.path.join(image_dir, map_key + ".jpg")
         obj_det = os.path.join(obj_dir, map_key + ".json")
@@ -31,4 +34,11 @@ def image_postprocessing(output_dir: str, image_df: pd.DataFrame):
             obj_json = json.load(f)
 
         # crop image with object detections
-        crop_image_flat(image, obj_json, output_dir)
+        crop_image_flat(image, obj_json, cropped_dir)
+
+
+if __name__ == "__main__":
+    path_for_dataset = Path(__file__).parents[2] / "data" / "example_dataset"
+    path_to_data_csv = Path(__file__).parents[2] / "data" / "example_dataset" / "data.csv"
+    data = pd.read_csv(path_to_data_csv)
+    crop_relevant_image_parts(path_for_dataset, data)
